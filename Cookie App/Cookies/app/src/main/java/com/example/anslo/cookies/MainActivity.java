6233f9d6a7bf9d07e573410851661ab611d9e937
PAULSOn109 <paulmburu53@gmail.com>
2018-04-03 23:06:03 +0300

    cookie app

---
+++ Cookie App/Cookies/app/src/main/java/com/example/anslo/cookies/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.anslo.cookies;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    /**
+     * Called when the cookie should be eaten
+     */
+    public void  eatCookie(View view){
+        // TODO: Find a reference to the ImageView in the layout. Change the image
+        // TODO: Find a reference to the TextView in the layout. Change the text
+        ImageView androidCookieImageView=(ImageView) findViewById(R.id.android_cookie_view);
+        androidCookieImageView.setImageResource(R.drawable.after_cookie);
+
+        TextView statusTextView = (TextView) findViewById(R.id.status_text_view);
+        statusTextView.setText("I'm so full");
+    }
+
+    /**
+     * Called when user wants another cookie
+     * @param view
+     */
+    public void anotherCookie(View view){
+        ImageView androidCookieImageView=(ImageView) findViewById(R.id.android_cookie_view);
+        androidCookieImageView.setImageResource(R.drawable.before_cookie);
+
+        TextView statusTextView = (TextView) findViewById(R.id.status_text_view);
+        statusTextView.setText("There you go !!!");
+    }
+}
